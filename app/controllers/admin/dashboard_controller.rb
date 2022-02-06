596a51a72f09fa182c5a3116b3708b3d6c87d5e5
SaranyaSagi <saranya108sagi@gmail.com>
2022-02-06 18:48:38 -0500

    add login authentication

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,6 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["NAME"] , password: ENV["PASSWORD"]
+  before_filter :authorize
 
   def show
     @product_count = Product.count
