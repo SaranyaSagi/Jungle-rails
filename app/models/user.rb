74d8667171feacc57a5a178f7a4620d8109cb5ef
SaranyaSagi <saranya108sagi@gmail.com>
2022-02-09 05:27:04 -0500

    merge feature/user-specs from Model spec user, validate user sign up, log in and authenticate with credentials, still have to fix name with first and last

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  #validates :first_name, presence: true
+  #validates :last_name, presence: true
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password_digest, presence: true
+  validates :password, length: { minimum: 6 }
+
+  def self.authenticate_with_credentials(email, password)
+    # @user = self.find_by_email(email.delete(' ')).try(:authenticate, password)
+    @user = self.where("lower(email) = ?", email.delete(' ').downcase).first.try(:authenticate, password)
+  end
 end
